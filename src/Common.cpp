08c559e025e81b8879759cf657bc488d26bbb776
maxnut <54683950+maxnut@users.noreply.github.com>
2024-04-09 18:30:23 +0200

    add show trajectory to cheats

--- src/Common.cpp
+++ src/Common.cpp
@@ -192,8 +192,9 @@ void Common::updateCheating()
 	bool instantComplete = Settings::get<bool>("level/instant_complete", false);
 	bool hitboxMultiplier = Settings::get<bool>("level/hitbox_multiplier", false);
 	bool layoutMode = Settings::get<bool>("level/layout_mode", false);
+	bool showTrajectory = Settings::get<bool>("player/show_trajectory/enabled", false);
 
-	if (speedhack != 1.f || Macrobot::playerMode == 0 || (showHitbox && !onDeath) || hidePause || hitboxMultiplier || noShaders || instantComplete || layoutMode)
+	if (speedhack != 1.f || Macrobot::playerMode == 0 || (showHitbox && !onDeath) || hidePause || hitboxMultiplier || noShaders || instantComplete || layoutMode || showTrajectory)
 	{
 		isCheating = true;
 		return;
